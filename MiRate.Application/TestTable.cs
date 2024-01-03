c5566ed38669473f9264ad95f7084ece46d55f46
Mohamad K Al-Absi <68118757+moba99@users.noreply.github.com>
2024-01-03 13:55:33 +0300

    push

---
+++ MiRate.Application/TestTable.cs
@@ -0,0 +1,14 @@
+﻿using Framework.Core.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiRate.Application
+{
+    public class TestTable : FullAuditedEntityBase<Guid>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ MiRate.Website/Areas/Identity/Data/MiRateWebsiteContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiRate.Website.Data;
+
+public class MiRateWebsiteContext : IdentityDbContext<IdentityUser>
+{
+    public MiRateWebsiteContext(DbContextOptions<MiRateWebsiteContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
